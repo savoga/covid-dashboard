99c240111c9e892a5540412c5fac9b2bae5149a7
savoga <savoure.gael@gmail.com>
2020-06-15 22:56:45 +0200

    dashboard template done

--- covid-app.py
+++ covid-app.py
@@ -17,15 +17,15 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 with urlopen('https://france-geojson.gregoiredavid.fr/repo/departements.geojson') as response:
     counties = json.load(response) # load map in json format
 
-df_map = pd.read_csv("departments_data.csv",
+df = pd.read_csv("data.csv",
                    dtype={"code": str})
 
-fig_map = px.choropleth(df_map, geojson=counties, color="data",
+fig_map = px.choropleth(df, geojson=counties, color="saturation",
                     locations="code", featureidkey="properties.code",
                     projection="mercator"
                    )
 fig_map.update_geos(fitbounds="locations", visible=False)
-fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, height=900)
 
 # ----------- TRENDLINE -----------
 
@@ -34,28 +34,26 @@ fig_trend = px.line(df_trend, x="day", y="value", title='Trend line')
 
 # ----------- BAR CHART -----------
 
-years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-         2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012]
-
 fig_bar = go.Figure()
-fig_bar.add_trace(go.Bar(x=years,
-                y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
-                   350, 430, 474, 526, 488, 537, 500, 439],
-                name='Rest of world',
-                marker_color='rgb(55, 83, 109)'
+fig_bar.add_trace(go.Bar(x=df['code'],
+                y=df['saturation'],
+                name='Saturation',
+                marker_color='rgb(55, 83, 109)',
+                orientation='h'
                 ))
-fig_bar.add_trace(go.Bar(x=years,
-                y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
-                   299, 340, 403, 549, 499],
-                name='China',
-                marker_color='rgb(26, 118, 255)'
+fig_bar.add_trace(go.Bar(x=df['code'],
+                y=df['saturation'],
+                name='Availability',
+                marker_color='rgb(26, 118, 255)',
+                orientation='h'
                 ))
+
 
 fig_bar.update_layout(
-    title='US Export of Plastic Scrap',
+    title='Saturation / Availability',
     xaxis_tickfont_size=14,
     yaxis=dict(
-        title='USD (millions)',
+        title='Saturation',
         titlefont_size=16,
         tickfont_size=14,
     ),
@@ -140,7 +138,12 @@ app.layout = html.Div(
                         dcc.Graph(figure=fig_map),
                         ], className="six columns"),
                     html.Div([
-                        dcc.Graph(figure=fig_trend)
+                        html.Div([
+                            dcc.Graph(figure=fig_trend)
+                            ], className="row"),
+                        html.Div([
+                            dcc.Graph(figure=fig_bar)
+                            ], className="row")
                         ], className="six columns")
             ], className="row")
     ])
