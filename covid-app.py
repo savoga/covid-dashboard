826da46ce3f47691038aeee19ca71e3342ca7c19
savoga <savoure.gael@gmail.com>
2020-06-17 14:25:27 +0200

    fix dropdown issue

--- covid-app.py
+++ covid-app.py
@@ -17,15 +17,15 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 with urlopen('https://france-geojson.gregoiredavid.fr/repo/departements.geojson') as response:
     counties = json.load(response) # load map in json format
 
-df = pd.read_csv("data.csv",
-                   dtype={"code": str})
+df_saturation = pd.read_excel("data.xls", sheet_name="saturation",
+                              dtype={'code': str})
 
-fig_map = px.choropleth(df, geojson=counties, color="saturation",
+fig_map = px.choropleth(df_saturation, geojson=counties, color="02/01/2020",
                     locations="code", featureidkey="properties.code",
-                    projection="mercator"
+                    projection="mercator", labels={'02/01/2020':'saturation'}
                    )
 fig_map.update_geos(fitbounds="locations", visible=False)
-fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, height=900)
+fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, height=850)
 
 # ----------- TRENDLINE -----------
 
@@ -35,14 +35,14 @@ fig_trend = px.line(df_trend, x="day", y="value", title='Trend line')
 # ----------- BAR CHART -----------
 
 fig_bar = go.Figure()
-fig_bar.add_trace(go.Bar(x=df['code'],
-                y=df['saturation'],
-                name='Saturation',
+fig_bar.add_trace(go.Bar(x=df_saturation['code'],
+                y=df_saturation['02/01/2020'],
+                name='02/01/2020',
                 marker_color='rgb(55, 83, 109)',
                 orientation='h'
                 ))
-fig_bar.add_trace(go.Bar(x=df['code'],
-                y=df['saturation'],
+fig_bar.add_trace(go.Bar(x=df_saturation['code'],
+                y=df_saturation['02/01/2020'],
                 name='Availability',
                 marker_color='rgb(26, 118, 255)',
                 orientation='h'
@@ -72,6 +72,7 @@ fig_bar.update_layout(
 
 app.layout = html.Div(
         [
+            ######## TITLE ##############"
             html.Div(
                 [
                 html.H1(
@@ -82,70 +83,100 @@ app.layout = html.Div(
                 )
             ]
         ),
+            #************** BUTTONS ****************
             html.Div(
+                ########## DATE ##########
+                [
+                    html.Div(
+                [
+                    html.Button('-', id='buttonMinus'),
+
+                    dcc.DatePickerSingle(
+                    id='date-picker-single',
+                    date=dt(2019, 1, 1)
+                        ),
+
+                    html.Button('+', id='buttonPlus'),
+                ], className="three columns"),
+
+                ########## DROPDOWN METRIC 1 ##########
+                    html.Div(
+                [
+                    dcc.Dropdown(
+                        id='metric',
+                        options=[
+                            {'label': 'Saturation', 'value': '0'},
+                            {'label': 'Number of private hospitals', 'value': '1'},
+                            ],
+                        style={
+                            'width': '250px',
+                            },
+                        value='0'
+                        )], className="three columns"),
+
+                ########## DROPDOWN METRIC 2 ##########
+                    html.Div(
+                [
+                    dcc.Dropdown(
+                        id='metric-3D',
+                        options=[
+                            {'label': 'Saturation', 'value': '0'},
+                            {'label': 'Number of private hospitals', 'value': '1'},
+                            ],
+                        style={
+                            'width': '250px',
+                            },
+                        value='1'
+                        ),
+                ], className="three columns"),
+
+                ########## DROPDOWN DEPARTMENT ##########
+                    html.Div(
+                [
+                    dcc.Dropdown(
+                        id='departments',
+                        options=[
+                            {'label': 'Manche', 'value': '50'},
+                            {'label': 'Morbihan', 'value': '56'}
+                            ],
+                        style={
+                            'width': '250px',
+                            },
+                        value=50
+                        )
+                    ], className="three columns")
+
+                ]),
+
+                #************** GRAPHS ****************
+
+                html.Div(
                 [
 
-                html.Button('-', id='buttonMinus'),
+                    ########## MAP ##########
 
-                dcc.DatePickerSingle(
-                id='date-picker-single',
-                date=dt(2019, 1, 1)
-                    ),
-
-                html.Button('+', id='buttonPlus'),
-
-                dcc.Dropdown(
-                    id='metric',
-                    options=[
-                        {'label': 'Saturation', 'value': '0'},
-                        {'label': 'Number of private hospitals', 'value': '1'},
-                        ],
-                    style={
-                        'width': '250px',
-                        },
-                    value='0'
-                    ),
-
-                dcc.Dropdown(
-                    id='metric-3D',
-                    options=[
-                        {'label': 'Saturation', 'value': '0'},
-                        {'label': 'Number of private hospitals', 'value': '1'},
-                        ],
-                    style={
-                        'width': '250px',
-                        },
-                    value='1'
-                    ),
-
-                dcc.Dropdown(
-                    id='departments',
-                    options=[
-                        {'label': 'Manche', 'value': '50'},
-                        {'label': 'Morbihan', 'value': '56'}
-                        ],
-                    style={
-                        'width': '250px',
-                        },
-                    value=50
-                    )
-
-                ], style={'columnCount': 4}
-            ),
-            html.Div(
-                [
                     html.Div([
+
                         dcc.Graph(figure=fig_map),
-                        ], className="six columns"),
+
+                        ], className="seven columns"),
+
+                    ########## TRENLINE ##########
+
                     html.Div([
+
                         html.Div([
                             dcc.Graph(figure=fig_trend)
                             ], className="row"),
+
+                    ########## BAR CHART ##########
+
                         html.Div([
                             dcc.Graph(figure=fig_bar)
                             ], className="row")
-                        ], className="six columns")
-            ], className="row")
+
+                        ], className="five columns")
+            ], className="eleven columns")
     ])
 
 if __name__ == '__main__':
